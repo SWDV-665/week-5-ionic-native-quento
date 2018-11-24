401aa9d666b4ebd6a4090322c10c7e6a018a112a
Quentin O <qochieng@gmail.com>
2018-11-24 12:36:15 -0600

    Project created
    Based on Wk4 progress

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Week 4 - Groceries App
+
+## CRUD Operations
+
+Groceries app should be able to perform create, read, update, and delete (CRUD).
+
+For Add Item - Use a drop downf or Quantity instead of an input field.
+
+Capture the screenshots of each tab or screencast walking through the flow of each tab.
+
+Create a folder called 'screenshots' or 'screencasts' and depending on what you generate. It is OK to have both screenshots and screencasts.
+
+Commit the generated screenshots or screencasts files in the GitHub assignment repo.
+  
---
+++ src/pages/home/home.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
+import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  title = "Grocery";
+
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: GroceriesServiceProvider, public dialogService: InputDialogServiceProvider) {
+
+  }
+
+  // Get initial groceries list from provider.
+  loadItems(){
+    return this.dataService.getItems();
+  }
+
+  removeItem(item, index){
+    // Troubleshooting
+    console.log("Deleting item .." + item.name, index);
+    
+    // Show item removal message (toast)
+    const toast = this.toastCtrl.create({
+      message: 'Deleting item - ' + item.name + " ...",
+      duration: 3000
+    });
+    toast.present();
+
+    // Use Groceries Provider to remove item
+    this.dataService.removeItem(index);
+    
+  }
+
+  editItem(item, index){
+    // Troubleshooting
+    console.log("Edit item .." + item.name, index);
+    
+    // Show item removal message (toast)
+    const toast = this.toastCtrl.create({
+      message: 'Editing item - ' + item.name + " ...",
+      duration: 3000
+    });
+    toast.present();
+
+    // Delete item from items array    
+    this.dialogService.showPrompt(item, index);
+  }
+
+  addItem(){
+    console.log("Item added"); 
+    this.dialogService.showPrompt();   
+  }
+
+}
